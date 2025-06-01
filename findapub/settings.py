c883e09c1f2b1d107a05eb1ed447d1ce4112a53d
Llywelyn1282 <gwilymllywelyn@gmail.com>
2025-06-01 18:48:02 +0100

    Add pub model

--- findapub/settings.py
+++ findapub/settings.py
@@ -24,12 +24,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h%4b3g@dya_qwjb=sh@+t64*9ev0xf7z=auxd6h%(8zx1ws@td'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = [
+    '.herokuapp.com',
+    '127.0.0.1'
+]
 
 
 # Application definition
@@ -79,16 +83,21 @@ WSGI_APPLICATION = 'findapub.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-#DATABASES = {
+# DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
-#}
+# }
 
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- pubs/models.py
+++ pubs/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+STATUS = ((0, "Draft"), (1, "Published"))
+YESORNO = ((0, "No"), (1, "Yes"))
+PRICE = ((0, "£"), (1, "££"), (2, "£££"), (3, "££££"), (4, "£££££"))
+WEEKDAYS = ((0, "Monday"), (1, "Tuesday"), (2, "Wednesday"), (3, "Thursday"), 
+                    (4, "Friday"), (5, "Saturday"), (6, "Sunday"))
+HOUR_OF_DAY_24 = [(i,i) for i in range(1,25)]
+
+
+class Pub(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    address = models.CharField(max_length=500, unique=True)
+    price = models.IntegerField(choices=PRICE, default=0)
+    food = models.IntegerField(choices=YESORNO, default=0)
+    craft_Beer = models.IntegerField(choices=YESORNO, default=0)
+    beer_Garden = models.IntegerField(choices=YESORNO, default=0)
+    dog_Friendly = models.IntegerField(choices=YESORNO, default=0)
+    pool_Table = models.IntegerField(choices=YESORNO, default=0)
+    dart_Board = models.IntegerField(choices=YESORNO, default=0)
+    description = models.TextField()
+    author = models.ForeignKey(
+            User, on_delete=models.CASCADE, related_name="pub_listing")
+    date_created = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
