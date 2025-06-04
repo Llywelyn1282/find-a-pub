6cb2304122da40a4bbe01c760fa98c5f48ec6f7d
Llywelyn1282 <gwilymllywelyn@gmail.com>
2025-06-04 09:38:20 +0100

    Link up summernote

--- pubs/admin.py
+++ pubs/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Pub, Comment, OpeningHour
+from django_summernote.admin import SummernoteModelAdmin
+
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
 
 
 class OpeningHoursInline(admin.TabularInline):
