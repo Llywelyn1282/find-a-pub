d36f0ee343eee2e82a7cc64119ea282e6d9044d9
Llywelyn1282 <gwilymllywelyn@gmail.com>
2025-06-05 12:00:47 +0100

    add post_detail view placeholder

---
+++ TESTING.md
@@ -0,0 +1,3 @@
+.phone_number was not appearing in database and throwing errors
+
+after many attempts the fix was deleting the migrations, caches and database data and starting again.
--- pubs/models.py
+++ pubs/models.py
@@ -23,6 +23,7 @@ DAYS_OF_WEEK = [
 class Pub(models.Model):
     name = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
+    image = models.ImageField(default="default.jpg")
     location = models.CharField(max_length=200, blank=True)
     address = models.CharField(max_length=500, unique=True)
     phone_number = models.CharField(max_length=12, blank=True)
