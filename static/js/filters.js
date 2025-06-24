55e1762e299fb5b5288d48cd158d60773eb4aee5
Llywelyn1282 <gwilymllywelyn@gmail.com>
2025-06-24 23:08:07 +0100

    add filter functionality 2

--- static/js/filters.js
+++ static/js/filters.js
@@ -6,7 +6,7 @@ const activeFilters = new Set();
 function updateFilters() {
   const filterArray = Array.from(activeFilters);
   const query = filterArray.length ? `?filters=${filterArray.join(",")}` : "";
-  window.location.href = "" + query;
+  window.location.href = "filter/" + query;
 }
 
 
@@ -33,7 +33,7 @@ filterButtons.forEach((button) => {
 clearButton.addEventListener("click", () => {
   filterButtons.forEach((btn) => btn.classList.remove("active"));
   activeFilters.clear();
-  window.location.href = "";
+  window.location.href = "filter/";
 });
 
 // Highlight Active Filters on Page Load
